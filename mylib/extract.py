94171d30b09415aa2cf669a890559fdbe7825835
Nruta <nruta.choudhari@duke.edu>
2024-10-07 11:59:23 -0400

    fixing test 2

--- mylib/extract.py
+++ mylib/extract.py
@@ -3,7 +3,7 @@ import requests
 
 def extract(
     data="biopics.csv",
-    file_path="data/Biopics.csv",
+    file_path="data/biopics.csv",
 ):
     with requests.get(data) as r:
         with open(file_path, "wb") as f:
